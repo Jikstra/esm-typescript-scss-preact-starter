61c1e934b9a5882efbdcf1e265b0fcf49035ec85
jikstra <jikstra@disroot.org>
2020-04-13 18:15:52 +0200

    Add size redution information to minify script

--- bin/minify-js.sh
+++ bin/minify-js.sh
@@ -2,8 +2,14 @@
 function minify-js() {
   FILE=$1
   FILE_SOURCE_MAP=$(basename $FILE).map
+  
   echo "+ minfiying $FILE"
+  size_before=$(stat --printf="%s" $FILE)
+  
   npx terser $FILE -m -c --source-map "content='$FILE.map',url='$FILE_SOURCE_MAP'" -o $FILE	
+  
+  size_after=$(stat --printf="%s" $FILE)
+  echo "+ size reduction: $((size_before - size_after))bytes $((1-$size_after/$size_before))%"
 }
 
 if [[ "$1" == "--help" || $1 == "" ]]; then
